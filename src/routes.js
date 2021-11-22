001c595fcddbc1afa53401ca068b840d0b80fc39
sonailagowrisankar <sonailagowrisankar@gmail.com>
2021-11-22 16:54:22 +0530

    new pages

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,9 @@ import Products from './pages/Products';
 // import Blog from './pages/Blog';
 import User from './pages/User';
 import NotFound from './pages/Page404';
+import Orders from './pages/Orders';
+import Vehicleb from './pages/Vehicle-booking';
+import Feedback from './pages/Feedback&review';
 
 // ----------------------------------------------------------------------
 
@@ -22,7 +25,10 @@ export default function Router() {
         { element: <Navigate to="/dashboard/app" replace /> },
         { path: 'app', element: <DashboardApp /> },
         { path: 'user', element: <User /> },
-        { path: 'products', element: <Products /> }
+        { path: 'products', element: <Products /> },
+        { path: 'orders', element: <Orders /> },
+        { path: 'vehicle-booking', element: <Vehicleb /> },
+        { path: 'feedback-review', element: <Feedback /> }
         // { path: 'blog', element: <Blog /> }
       ]
     },
