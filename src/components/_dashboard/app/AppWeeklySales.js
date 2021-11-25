9871ed7fa31d0918ff046146951c0f3cbdc88594
sonailagowrisankar <sonailagowrisankar@gmail.com>
2021-11-25 17:29:16 +0530

    working on styled props

--- src/components/_dashboard/app/AppWeeklySales.js
+++ src/components/_dashboard/app/AppWeeklySales.js
@@ -16,6 +16,7 @@ const RootStyle = styled(Card)(({ theme }) => ({
   padding: theme.spacing(5, 0),
   color: theme.palette.primary.darker,
   backgroundColor: theme.palette.primary.lighter
+  // backgroundColor: props.bc
 }));
 
 const OverCard = styled(Card)(({ theme }) => ({
@@ -50,10 +51,11 @@ const IconWrapperStyle = styled('div')(({ theme }) => ({
 
 const TOTAL = 70;
 
-export default function AppWeeklySales() {
+export default function AppWeeklySales(props) {
   const [card, setCard] = useState(false);
+  console.log(props)
   return (
-    <RootStyle onMouseOver={() => { setCard(true) }} onMouseLeave={() => { setCard(false) }}>
+    <RootStyle onMouseOver={() => { setCard(true) }} onMouseLeave={() => { setCard(false) }} bc = {props.bc}>
   
       <IconWrapperStyle>
 
@@ -62,25 +64,25 @@ export default function AppWeeklySales() {
       <div style={{height:"70px"}}>
       {!card ?
         <>
-          <Typography variant="h3">{fShortenNumber(TOTAL)}</Typography>
+          <Typography variant="h3">{fShortenNumber(props.count)}</Typography>
           <Typography variant="subtitle2" sx={{ opacity: 0.72 }}>
-            Weekly Sales
+            {props.tagName}
           </Typography>
         </>
         :
         <div className="weekly-sales">
           <div>
-            <p>12</p>
+            <p>{props.counts[0]}</p>
             <p>today</p>
           </div>
 
           <div>
-            <p>120</p>
+            <p>{props.counts[1]}</p>
             <p>week</p>
           </div>
 
           <div>
-            <p>123</p>
+            <p>{props.counts[2]}</p>
             <p>month</p>
           </div>
         </div>
