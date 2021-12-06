544d4bcbacf1f420a1d1dddf6c116a66653ba1f6
modernsilpi <modernsilpi@gmail.com>
2021-12-06 10:53:27 +0530

    mobx multiple stores, api methods, initilization completed

---
+++ src/state_management/store/store2.js
@@ -0,0 +1,16 @@
+import { makeAutoObservable } from 'mobx'
+
+class Store2 {
+
+    qty = 0;
+
+    constructor(){
+        makeAutoObservable(this)
+    }
+
+    incrementQty(){
+        this.qty +=1;
+    }
+
+}
+export default  Store2;
