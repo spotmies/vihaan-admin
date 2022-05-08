5f777c70670383bcbe08a5c0c7263f6a2d497bfc
sekharjavvadi <sekharatece@gmail.com>
2022-05-08 09:04:36 +0530

    small changes made

--- src/layouts/dashboard/SidebarConfig.js
+++ src/layouts/dashboard/SidebarConfig.js
@@ -3,8 +3,6 @@ import pieChart2Fill from "@iconify/icons-eva/pie-chart-2-fill";
 import peopleFill from "@iconify/icons-eva/people-fill";
 import shoppingBagFill from "@iconify/icons-eva/shopping-bag-fill";
 import fileTextFill from "@iconify/icons-eva/file-text-fill";
-import lockFill from "@iconify/icons-ic/motorcycle";
-import test from "@iconify/icons-ic/round-bike-scooter";
 import plus from "@iconify/icons-eva/plus-circle-fill";
 import feedback from "@iconify/icons-ic/comment";
 
@@ -33,32 +31,22 @@ const sidebarConfig = [
     path: "/dashboard/products",
     icon: getIcon(shoppingBagFill),
   },
-  // {
-  //   title: "Vehicle booking",
-  //   path: "/dashboard/vehicle-booking",
-  //   icon: getIcon(lockFill),
-  // },
   {
-    title: "Test ride booking",
-    path: "/dashboard/test-ride-booking",
-    icon: getIcon(test),
+    title: "Add new product",
+    path: "/dashboard/new-product",
+    icon: getIcon(plus),
   },
-  // {
-  //   title: "Add new product",
-  //   path: "/dashboard/new-product",
-  //   icon: getIcon(plus),
-  // },
   {
     title: "Feedback & reviews",
     path: "/dashboard/feedback-review",
     icon: getIcon(feedback),
   },
 
-  {
-    title: "login",
-    path: "/login",
-    icon: getIcon(lockFill),
-  },
+  // {
+  //   title: "login",
+  //   path: "/login",
+  //   icon: getIcon(lockFill),
+  // },
   // {
   //   title: 'register',
   //   path: '/register',
